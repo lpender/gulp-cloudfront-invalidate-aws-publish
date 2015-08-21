50e0ba9938f63c4aa2aa6d3b08468603fd3c7f5a
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-08-21 21:48:42 +0530

    Version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # gulp-cloudfront-invalidate
-A gulp plugin that allows you to invalidate paths in cloudfront
+A gulp plugin that allows you to invalidate paths in AWS cloudfront
+
+## Installation
+```
+npm install gulp-cloudfront-invalidate
+```
+
+## Usage
+
+```js
+var gulp = require('gulp')
+  , cloudfront = require('gulp-cloudfront-invalidate');
+
+var settings = {
+  distribution: 'E2A654H2YRPD0W', // Cloudfront distribution ID
+  paths: ['index.html']           // Paths to invalidate
+  accessKeyId: '...',             // AWS Access Key ID
+  secretAccessKey: '...',         // AWS Secret Access Key
+  wait: true                      // Whether to wait until invalidation is completed (default: false)
+}
+
+gulp.task('invalidate', function () {
+  return gulp.src('*')
+    .pipe(cloudfront(settings));
+});
+```
+
+
+If you like this project, please watch this and follow me.
+
+## Contributors
+Here is a list of [Contributors](http://github.com/pksunkara/gulp-cloudfront-invalidate/contributors)
+
+__I accept pull requests and guarantee a reply back within a day__
+
+## License
+MIT/X11
+
+## Bug Reports
+Report [here](http://github.com/pksunkara/gulp-cloudfront-invalidate/issues). __Guaranteed reply within a day__.
+
+## Contact
+Pavan Kumar Sunkara (pavan.sss1991@gmail.com)
+
+Follow me on [github](https://github.com/users/follow?target=pksunkara), [twitter](http://twitter.com/pksunkara)
---
+++ index.js
@@ -0,0 +1,63 @@
+var util = require('gulp-util')
+  , through = require('through2')
+  , aws = require('aws-sdk');
+
+module.exports = function (options) {
+  options.wait = (options.wait === undefined || !options.wait) ? false : true;
+
+  var cloudfront = new aws.CloudFront();
+
+  cloudfront.config.update({
+    accessKeyId: options.accessKeyId || process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: options.secretAccessKey || process.env.AWS_SECRET_ACCESS_KEY
+  });
+
+  var complain = function (err, msg, callback) {
+    throw new util.PluginError('gulp-cloudfront-invalidate', msg + ': ' + err);
+    return callback(false);
+  };
+
+  var check = function (id, callback) {
+    cloudfront.getInvalidation({
+      DistributionId: options.distribution,
+      Id: id
+    }, function (err, res) {
+      if (err) return complain(err, 'Could not check on invalidation', callback);
+
+      if (res.Invalidation.Status === 'Completed') {
+        return callback();
+      } else {
+        setTimeout(function () {
+          check(id, callback);
+        }, 1000);
+      }
+    })
+  };
+
+  var work = function (callback) {
+    cloudfront.createInvalidation({
+      DistributionId: options.distribution,
+      InvalidationBatch: {
+        CallerReference: Date.now().toString(),
+        Paths: {
+          Quantity: options.paths.length,
+          Items: options.paths
+        }
+      }
+    }, function (err, res) {
+      if (err) return complain(err, 'Could not invalidate cloudfront', callback);
+
+      util.log('Cloudfront invalidation created: ' + res.Invalidation.Id);
+
+      if (!options.wait) {
+        return callback();
+      }
+
+      check(res.Invalidation.Id, callback);
+    });
+  };
+
+  return through.obj(function (file, enc, callback) {
+    callback();
+  }, work);
+};
